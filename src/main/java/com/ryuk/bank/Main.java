6e843862153fe3b5da034e8024225bf6b2f92c03
RyukaTV63 <gabinjuillardpro@gmail.com>
2025-04-20 14:04:26 +0200

    init repository

---
+++ src/main/java/com/ryuk/bank/Main.java
@@ -0,0 +1,42 @@
+package com.ryuk.bank;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	@SuppressWarnings("exports")
+	@Override
+	public void start(Stage primaryStage) {
+        final VBox loginForm = new VBox(10);
+        loginForm.getChildren().addAll(new Label("Username :"), new Button("Connexion"));
+        loginForm.setAlignment(Pos.CENTER);
+        loginForm.setPrefSize(200, 250);
+
+        final Group loginContainer = new Group(loginForm);
+        final StackPane center = new StackPane(loginContainer);
+        center.setAlignment(Pos.CENTER);
+        final BorderPane root = new BorderPane();
+        root.setCenter(center);
+
+        final Scene scene = new Scene(root, 800, 500);
+        scene.getStylesheets().add(getClass().getResource("/css/login.css").toExternalForm());
+        primaryStage.setTitle("Bank App");
+        primaryStage.setMaximized(true);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module com.ryuk.bank {
+	exports com.ryuk.bank;
+
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.graphics;
+}
