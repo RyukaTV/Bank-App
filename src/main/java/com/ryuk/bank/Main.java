97bd102149e5a2c3ecdd0a3c4ea6cec43155397e
RyukaTV63 <gabinjuillardpro@gmail.com>
2025-04-20 14:13:53 +0200

    added new field in login form

--- src/main/java/com/ryuk/bank/Main.java
+++ src/main/java/com/ryuk/bank/Main.java
@@ -6,6 +6,8 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -17,9 +19,15 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
         final VBox loginForm = new VBox(10);
-        loginForm.getChildren().addAll(new Label("Username :"), new Button("Connexion"));
         loginForm.setAlignment(Pos.CENTER);
         loginForm.setPrefSize(200, 250);
+        
+        final TextField tf= new TextField();
+        
+        final PasswordField pf= new PasswordField();
+        
+        loginForm.getChildren().addAll(new Label("Identifiant :"), tf, new Label("Mot de passe:"), pf, new Button("Connexion"));
+        
 
         final Group loginContainer = new Group(loginForm);
         final StackPane center = new StackPane(loginContainer);
